aa8f770807e131e1f8a77503b6e8f67588183bab
ttngu <tammyn137@gmail.com>
2020-12-30 15:46:28 -0800

    added scroll to top button, styling and functionality

--- main.js
+++ main.js
@@ -19,3 +19,15 @@
 //         // }
 //     })
 // })
+
+
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function() {
+    console.log("click!")
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
