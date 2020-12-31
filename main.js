428a1a9ab9e4d070608cf9b05f3472bf1015cac6
ttngu <tammyn137@gmail.com>
2020-12-30 17:27:40 -0800

    updated nav again

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 // To Top Button
-// const btnScrollToTop = document.querySelector("#btnScrollToTop");
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
 
-// btnScrollToTop.addEventListener("click", function() {
-//     console.log("click!");
-//     window.scrollTo({
-//         top: 0,
-//         left: 0,
-//         behavior: "smooth"
-//     });
-// });
-
-
-$(document).ready(function() {
-  
-    var scrollLink = $('.scroll');
-    
-    // Smooth scrolling
-    scrollLink.click(function(e) {
-      e.preventDefault();
-      $('body,html').animate({
-        scrollTop: $(this.hash).offset().top
-      }, 1000 );
+btnScrollToTop.addEventListener("click", function() {
+    console.log("click!");
+    window.scrollTo({
+        top: 60,
+        left: 0,
+        behavior: "smooth"
     });
-})
+});
+
+// WHY DOESNT THIS WORK?!
+// $(document).ready(function() {
+  
+//     var scrollLink = $('.scroll');
+    
+//     // Smooth scrolling
+//     scrollLink.click(function(e) {
+//       e.preventDefault();
+//       $('body,html').animate({
+//         scrollTop: $(this.hash).offset().top
+//       }, 1000 );
+//     });
+// })
