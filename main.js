03e8c3fe378363d943270228df7c54f2342388a2
ttngu <tammyn137@gmail.com>
2020-12-30 17:18:15 -0800

    sticky nav with image

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
-// let portf = document.getElementById("portf");
+// To Top Button
+// const btnScrollToTop = document.querySelector("#btnScrollToTop");
 
-// // function scrollToTop() {
-// //   elmnt.scrollIntoView(true); // Portfolio
-// // }
-
-// $(function() {
-//     $('.navbar a').on('click', function() {
-//         const hashtag = $(this.hash);
-
-//         // hashtag = hashtag.length ? hashtag : $('name=' + this.has.substr(1) + ']');
-
-//         // if (hashtag.length) {
-//             $('html, body').animate({
-//                 scrollTop: $(hashtag).offset().Top
-//             }, 500);
-
-//         //     return false;
-//         // }
-//     })
-// })
+// btnScrollToTop.addEventListener("click", function() {
+//     console.log("click!");
+//     window.scrollTo({
+//         top: 0,
+//         left: 0,
+//         behavior: "smooth"
+//     });
+// });
 
 
-const btnScrollToTop = document.querySelector("#btnScrollToTop");
-
-btnScrollToTop.addEventListener("click", function() {
-    console.log("click!")
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
+$(document).ready(function() {
+  
+    var scrollLink = $('.scroll');
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000 );
     });
-});
+})
